71bcb4e9aa8f02eb65f65bed64867c080e56ffe6
Cedric <99885260+Cenikl@users.noreply.github.com>
2023-06-15 11:56:20 +0300

    fix localStorage value type

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
-const useLocalStorage = <T>(key: string, initialValue: T): [T, (value: T) => void] => {
-  const [value, setValue] = useState<T>(() => {
+const useLocalStorage = (key: string, initialValue: any)=> {
+  const [value, setValue] = useState(() => {
     if (typeof window !== 'undefined') {
       const storedValue = localStorage.getItem(key);
       return storedValue ? JSON.parse(storedValue) : initialValue;
